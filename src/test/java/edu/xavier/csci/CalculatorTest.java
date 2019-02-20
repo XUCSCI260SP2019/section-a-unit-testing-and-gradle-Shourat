5de25da221a540df4ae817afbbb83d30996eaa48
Shourat Sandhawalia <shouratsandhawalia@Shourats-MacBook-Pro.local>
2019-02-19 23:24:51 -0500

    src/test/java/edu/xavier/csci/CalculatorTest.java

--- src/test/java/edu/xavier/csci/CalculatorTest.java
+++ src/test/java/edu/xavier/csci/CalculatorTest.java
@@ -1,4 +1,17 @@
 package test.java.edu.xavier.csci;
 
-public class CalculatorTest {
+import org.junit.Test;
+
+import static org.junit.Assert.*;.;
+
+public class add{
+    @Test
+    public void Test() {
+        Calculator classUnderTest = new Calculator();
+        assertNotNull(classUnderTest.Calculator());
+        int output = classUnderTest..add(1,2);
+        assertEquals(3, output);
+    }
+
+
 }
